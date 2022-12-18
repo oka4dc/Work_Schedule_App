2db74da81b7cfb3cbed18b6327406cab924426c8
unknown <Dennis Okafor>
2022-12-18 12:32:36 +0100

    created the templates files and the Url routing

--- Work_Schedule_Project/WorkApp/views.py
+++ Work_Schedule_Project/WorkApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Staff, Staff_Grade
 
 # Create your views here.
+def index(request):
+    staffs=Staff.objects.all()
+    context={
+        'staff':staffs
+    }
+    return render(request, 'Home.html', context)
